397b1e20f3c2ed0d0dd80e546754c4852fd70f2c
Benlaptrinh <nguyenhuuviet159@gmail.com>
2026-01-13 15:40:41 +0700

    ok

---
+++ src/etl.py
@@ -0,0 +1,47 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, to_date
+
+# 1. Khởi tạo Spark
+spark = SparkSession.builder \
+    .appName("PM25_ETL") \
+    .getOrCreate()
+
+# 2. Đường dẫn dữ liệu
+RAW_PATH = "data/raw/pm25_sensor_5049_page_1.json"
+OUTPUT_PATH = "data/processed/pm25_clean"
+
+# 3. Đọc JSON
+df_raw = spark.read.json(RAW_PATH)
+
+# 4. Flatten dữ liệu (results là array)
+df = df_raw.selectExpr("explode(results) as r")
+
+# 5. Chọn field cần thiết
+df_selected = df.select(
+    col("r.value").alias("pm25"),
+    col("r.period.datetimeTo.utc").alias("datetime"),
+    col("r.parameter.name").alias("parameter")
+)
+
+# 6. Lọc dữ liệu PM2.5 hợp lệ
+df_clean = df_selected.filter(
+    (col("parameter") == "pm25") &
+    (col("pm25") > 0) &
+    (col("pm25") != -999)
+)
+
+# 7. Chuẩn hóa ngày
+df_final = df_clean.withColumn(
+    "date",
+    to_date(col("datetime"))
+).select(
+    "date",
+    "pm25"
+)
+
+# 8. Ghi ra Parquet
+df_final.write.mode("overwrite").parquet(OUTPUT_PATH)
+
+print("ETL DONE. Output saved to:", OUTPUT_PATH)
+
+spark.stop()
